dde9bb6c1c6fd2a5428011fc47a72aba97b2b6a9
aipsel <yuyaoyang09@gmail.com>
2016-03-24 17:37:26 +0800

    Add: Parse SDK.

--- app/src/main/java/xyz/imyeo/learnc/core/Application.java
+++ app/src/main/java/xyz/imyeo/learnc/core/Application.java
@@ -1,6 +1,10 @@
 package xyz.imyeo.learnc.core;
 
-public class Application extends android.app.Application{
+import com.parse.Parse;
+
+import xyz.imyeo.learnc.R;
+
+public class Application extends android.app.Application {
 
     private static Application mInstance;
 
@@ -8,6 +12,12 @@ public class Application extends android.app.Application{
     public void onCreate() {
         super.onCreate();
         mInstance = this;
+
+        // Initialize Parse SDK.
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getResources().getString(R.string.parse_app_id))
+                .server(getResources().getString(R.string.parse_server))
+                .build());
     }
 
     public static Application get() {
